d4658ac9761e5d0f009bf1e5d45bf545c9a0e49c
ferrajunianwahidna <64394822+ferrajunianwahidna@users.noreply.github.com>
2020-04-27 21:50:46 +0700

    Add files via upload

---
+++ Instructor.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+public class Instructor {
+    private String lastName;
+    private String firstName;
+    private String officeNumber;
+    
+    public Instructor(String lname, String fname, String office) {
+        lastName = lname;
+        firstName = fname;
+        officeNumber = office;
+    }
+    
+    public Instructor(Instructor object2) {
+        lastName = object2.lastName;
+        firstName = object2.firstName;
+        officeNumber = object2.officeNumber;
+    }
+    
+    public void set(String lname, String fname, String office) {
+        lastName = lname;
+        firstName = fname;
+        officeNumber = office;
+    }
+    
+    public String toString() {
+        String str = "Last Name: " + lastName + 
+                "\nFirst Name: " + firstName + 
+                "\nOffice Number: " + officeNumber;
+        return str;
+    }
+}
